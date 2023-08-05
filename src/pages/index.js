526861ad970a8cd3fb7c829af7ac2f4f9d3aead5
VishnuKMi <vishnuarley@gmail.com>
2023-08-05 13:22:14 +0530

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,182 +1,9 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-// import Web3Modal from 'web3modal'
-// import { providers, Contract } from 'ethers'
-// import { useEffect, useRef, useState, useCallback } from 'react'
-// import { WHITELIST_CONTRACT_ADDRESS, abi } from '../pages/constantIndex'
-
-// export default function Home () {
-//   const [walletConnected, setWalletConnected] = useState(false)
-//   const [joinedWhitelist, setJoinedWhitelist] = useState(false)
-//   const [loading, setLoading] = useState(false)
-//   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0)
-//   const web3ModalRef = useRef()
-
-//   const getProviderOrSigner = async (needSigner = false) => {
-//     const provider = await web3ModalRef.current.connect()
-//     const web3Provider = new providers.Web3Provider(provider)
-
-//     const { chainId } = await web3Provider.getNetwork()
-//     if (chainId !== 11155111) {
-//       window.alert('Change the network to Sepolia')
-//       throw new Error('Change network to Sepolia')
-//     }
-
-//     if (needSigner) {
-//       const signer = web3Provider.getSigner()
-//       return signer
-//     }
-//     return web3Provider
-//   }
-
-//   // Memoized getNumberOfWhitelisted function
-//   const getNumberOfWhitelistedCallback = useCallback(async () => {
-//     try {
-//       const provider = await getProviderOrSigner()
-//       const whitelistContract = new Contract(
-//         WHITELIST_CONTRACT_ADDRESS,
-//         abi,
-//         provider
-//       )
-//       const _numberOfWhitelisted =
-//         await whitelistContract.numAddressesWhitelisted()
-//       setNumberOfWhitelisted(_numberOfWhitelisted)
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }, [])
-
-//   // Memoized checkIfAddressInWhitelist function
-//   const checkIfAddressInWhitelistCallback = useCallback(async () => {
-//     try {
-//       const signer = await getProviderOrSigner(true)
-//       const whitelistContract = new Contract(
-//         WHITELIST_CONTRACT_ADDRESS,
-//         abi,
-//         signer
-//       )
-//       const address = await signer.getAddress()
-//       const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
-//         address
-//       )
-//       setJoinedWhitelist(_joinedWhitelist)
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }, [])
-
-//   // Memoized connectWallet function
-//   const connectWalletCallback = useCallback(async () => {
-//     try {
-//       await getProviderOrSigner()
-//       setWalletConnected(true)
-
-//       checkIfAddressInWhitelistCallback()
-//       getNumberOfWhitelistedCallback()
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }, [checkIfAddressInWhitelistCallback, getNumberOfWhitelistedCallback])
-
-//   useEffect(() => {
-//     if (!walletConnected) {
-//       web3ModalRef.current = new Web3Modal({
-//         network: 'goerli',
-//         providerOptions: {},
-//         disableInjectedProvider: false
-//       })
-//       connectWalletCallback()
-//     }
-//   }, [walletConnected, connectWalletCallback])
-
-//   const addAddressToWhitelist = async () => {
-//     try {
-//       const signer = await getProviderOrSigner(true)
-//       const whitelistContract = new Contract(
-//         WHITELIST_CONTRACT_ADDRESS,
-//         abi,
-//         signer
-//       )
-//       const tx = await whitelistContract.addAddressToWhitelist()
-//       setLoading(true)
-//       await tx.wait()
-//       setLoading(false)
-//       await getNumberOfWhitelisted()
-//       setJoinedWhitelist(true)
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }
-
-//   const renderButton = () => {
-//     if (walletConnected) {
-//       if (joinedWhitelist) {
-//         return (
-//           <div className={styles.description}>
-//             Thanks for joining the Whitelist!
-//           </div>
-//         )
-//       } else if (loading) {
-//         return <button className={styles.button}>Loading...</button>
-//       } else {
-//         return (
-//           <button onClick={addAddressToWhitelist} className={styles.button}>
-//             Join the Whitelist
-//           </button>
-//         )
-//       }
-//     } else {
-//       return (
-//         <button onClick={connectWalletCallback} className={styles.button}>
-//           Connect your wallet
-//         </button>
-//       )
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <Head>
-//         <title>Whitelist Dapp</title>
-//         <meta name='description' content='Whitelist-Dapp' />
-//         <link rel='icon' href='/favicon.ico' />
-//       </Head>
-//       <div className={styles.main}>
-//         <div>
-//           <h1 className={styles.title}>
-//             Welcome to BlockMaxi&#39;s Inventory!
-//           </h1>
-//           <div className={styles.description}>
-//             {/* Using HTML Entities for the apostrophe */}
-//             It&#39;s an NFT collection for CryptoHeads 💀
-//           </div>
-//           <div className={styles.description}>
-//             {numberOfWhitelisted} have already joined the Whitelist
-//           </div>
-//           {renderButton()}
-//         </div>
-//         <div>
-//           <Image
-//             alt='Crypto Devs'
-//             className={styles.image}
-//             src='crypto-devs.svg'
-//             width={500}
-//             height={500}
-//           />
-//         </div>
-//       </div>
-
-//       <footer className={styles.footer}>Made with &#10084; by BlockMaxi</footer>
-//     </div>
-//   )
-// }
-
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Web3Modal from 'web3modal'
 import { providers, Contract } from 'ethers'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState, useCallback } from 'react'
 import { WHITELIST_CONTRACT_ADDRESS, abi } from '../pages/constantIndex'
 
 export default function Home () {
@@ -209,7 +36,7 @@ export default function Home () {
     const provider = await web3ModalRef.current.connect()
     const web3Provider = new providers.Web3Provider(provider)
 
-    // If user is not connected to the Goerli network, let them know and throw an error
+    // If the user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork()
     if (chainId !== 11155111) {
       window.alert('Change the network to Sepolia')
@@ -237,13 +64,13 @@ export default function Home () {
         abi,
         signer
       )
-      // call the addAddressToWhitelist from the contract
+      // Call the addAddressToWhitelist from the contract
       const tx = await whitelistContract.addAddressToWhitelist()
       setLoading(true)
-      // wait for the transaction to get mined
+      // Wait for the transaction to get mined
       await tx.wait()
       setLoading(false)
-      // get the updated number of addresses in the whitelist
+      // Get the updated number of addresses in the whitelist
       await getNumberOfWhitelisted()
       setJoinedWhitelist(true)
     } catch (err) {
@@ -252,7 +79,7 @@ export default function Home () {
   }
 
   /**
-   * getNumberOfWhitelisted:  gets the number of whitelisted addresses
+   * getNumberOfWhitelisted: Gets the number of whitelisted addresses
    */
   const getNumberOfWhitelisted = async () => {
     try {
@@ -266,7 +93,7 @@ export default function Home () {
         abi,
         provider
       )
-      // call the numAddressesWhitelisted from the contract
+      // Call the numAddressesWhitelisted from the contract
       const _numberOfWhitelisted =
         await whitelistContract.numAddressesWhitelisted()
       setNumberOfWhitelisted(_numberOfWhitelisted)
@@ -276,22 +103,22 @@ export default function Home () {
   }
 
   /**
-   * checkIfAddressInWhitelist: Checks if the address is in whitelist
+   * checkIfAddressInWhitelist: Checks if the address is in the whitelist
    */
   const checkIfAddressInWhitelist = async () => {
     try {
       // We will need the signer later to get the user's address
       // Even though it is a read transaction, since Signers are just special kinds of Providers,
-      // We can use it in it's place
+      // We can use it in its place
       const signer = await getProviderOrSigner(true)
       const whitelistContract = new Contract(
         WHITELIST_CONTRACT_ADDRESS,
         abi,
         signer
       )
-      // Get the address associated to the signer which is connected to  MetaMask
+      // Get the address associated with the signer which is connected to MetaMask
       const address = await signer.getAddress()
-      // call the whitelistedAddresses from the contract
+      // Call the whitelistedAddresses from the contract
       const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
         address
       )
@@ -301,13 +128,11 @@ export default function Home () {
     }
   }
 
-  /*
-    connectWallet: Connects the MetaMask wallet
-  */
-  const connectWallet = async () => {
+  /**
+   * Memoized connectWallet function
+   */
+  const connectWalletCallback = useCallback(async () => {
     try {
-      // Get the provider from web3Modal, which in our case is MetaMask
-      // When used for the first time, it prompts the user to connect their wallet
       await getProviderOrSigner()
       setWalletConnected(true)
 
@@ -316,11 +141,28 @@ export default function Home () {
     } catch (err) {
       console.error(err)
     }
-  }
+  }, [checkIfAddressInWhitelist, getNumberOfWhitelisted])
 
-  /*
-    renderButton: Returns a button based on the state of the dapp
-  */
+  // useEffects are used to react to changes in the state of the website
+  // The array at the end of the function call represents what state changes will trigger this effect
+  // In this case, whenever the value of `walletConnected` changes - this effect will be called
+  useEffect(() => {
+    // If the wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting its `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: 'goerli',
+        providerOptions: {},
+        disableInjectedProvider: false
+      })
+      connectWalletCallback()
+    }
+  }, [walletConnected, connectWalletCallback])
+
+  /**
+   * Returns a button based on the state of the dapp
+   */
   const renderButton = () => {
     if (walletConnected) {
       if (joinedWhitelist) {
@@ -340,29 +182,12 @@ export default function Home () {
       }
     } else {
       return (
-        <button onClick={connectWallet} className={styles.button}>
+        <button onClick={connectWalletCallback} className={styles.button}>
           Connect your wallet
         </button>
       )
     }
   }
-
-  // useEffects are used to react to changes in state of the website
-  // The array at the end of function call represents what state changes will trigger this effect
-  // In this case, whenever the value of `walletConnected` changes - this effect will be called
-  useEffect(() => {
-    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
-    if (!walletConnected) {
-      // Assign the Web3Modal class to the reference object by setting it's `current` value
-      // The `current` value is persisted throughout as long as this page is open
-      web3ModalRef.current = new Web3Modal({
-        network: 'goerli',
-        providerOptions: {},
-        disableInjectedProvider: false
-      })
-      connectWallet()
-    }
-  }, [walletConnected])
 
   return (
     <div>
@@ -386,7 +211,7 @@ export default function Home () {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src='./crypto-devs.svg' />
+          <Image alt='image' className={styles.image} src='./crypto-devs.svg' />
         </div>
       </div>
 
